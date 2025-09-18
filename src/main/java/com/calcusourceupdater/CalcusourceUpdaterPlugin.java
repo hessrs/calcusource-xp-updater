4904da7c0cfa699848e86f58993f2908a489813b
hessrs <hessrunelite@gmail.com>
2025-09-17 20:40:56 -0600

    Make number long

--- src/main/java/com/calcusourceupdater/CalcusourceUpdaterPlugin.java
+++ src/main/java/com/calcusourceupdater/CalcusourceUpdaterPlugin.java
@@ -93,7 +93,7 @@ public class CalcusourceUpdaterPlugin extends Plugin
 				update(local.getName());
 				lastXp = totalXp;
 			}
-			else if (totalXp >= 4600000000)
+			else if (totalXp >= 4600000000L)
 			{
 			    update(local.getName());
 			}
